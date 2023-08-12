b7f9bc042e2937d1e5fe6fadedd46f35b5b4fd5e
kevohk4 <kodhiambo170@gmail.com>
2023-08-12 22:59:20 +0300

    31t677Top

---
+++ time/delay.py
@@ -0,0 +1,18 @@
+import time
+print("a")
+time.sleep(1)
+print("l")
+time.sleep(1)
+print("i")
+time.sleep(1)
+print("v")
+time.sleep(1)
+print("i")
+time.sleep(1)
+print("t")
+time.sleep(1)
+print("s")
+time.sleep(1)
+print("a")
+
+
